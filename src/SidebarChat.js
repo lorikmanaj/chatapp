1ff489046d39238d6eeeebb8a48f1233b0dcc652
lorikmanaj <lorikmanaj@hotmail.com>
2021-04-24 05:38:45 +0200

    Enabled google auth firebase

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -5,13 +5,23 @@ import { Avatar } from "@material-ui/core";
 import './SidebarChat.css';
 
 import db from "./firebase";
+import {Link} from 'react-router-dom';
 
 function SidebarChat({ addNewChat }) {
 
     const [seed, setSeed] = useState("");
+    const [messages, setMessages] = useState("");
+    
+    useEffect(() => {
+        if(id){
+            db.collection('rooms').doc(id).collection('messages').orderBy('timestamp','desc').onSnapshot(snapshot => {
+                setMessages(snapshot.docs.map((doc) => doc.data()))
+            })
+        }
+    }, [id]);
 
     useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000));
+        setSeed(Math.floor(Math.random() * 5000));        
     }, []);
 
     const createChat = () => {
@@ -25,18 +35,19 @@ function SidebarChat({ addNewChat }) {
     };
 
     return !addNewChat ? (
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}}.svg`} />
-            <div className="sidebarChat_info">
-                <h2>Room name</h2>
-                <p>Last Message...</p>
+        <Link to={`/rooms/${id}`} key={id}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}}.svg`} />
+                <div className="sidebarChat_info">
+                    <h2>Room name</h2>
+                    <p>Last Message...</p>
+                </div>
             </div>
-        </div>
+        </Link>
     ) : (
-        <div onClick={createChat}
-            className="sidebarChat">
-                <h2>Add new chat</h2>
-            </div>
+        <div onClick={createChat} className="sidebarChat">
+            <h3 className="add-new-chat-title">Add New Chat</h3>
+        </div>
     )
 }
 
