c0e75186c080ec4c6a856548d5cd2f95489918e6
lorikmanaj <lorikmanaj@hotmail.com>
2021-04-24 05:03:09 +0200

    Changed sidebar.js regarding firebase logic for chat rooms

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,9 +11,26 @@ import {SearchOutlined} from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
 
 import './Sidebar.css';
+
+import db from './firebase';
+import { useStateValue } from './StateProvider';
+
 import PropTypes from 'prop-types';
 
 function Sidebar() {
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot => (
+            setRooms(snapshot.docs.map(doc => 
+                ({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            ))
+        ))
+    }, []);
+
     return (
         <div className="sidebar">
             <div className="sidebar_header">
